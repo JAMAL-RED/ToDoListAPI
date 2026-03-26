29639d997279981cb5ec0915feee788ef3411c2a
jamal09-hub <elder.junior@aluno.senai.br>
2026-03-26 13:40:06 -0300

    feat: Estrutura para as tabelas e migrations

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models.Entities;
+
+namespace ToDoList.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        // Passo 1 Criar entidades (Models/Entities/Usuario)
+        //Passo 2 'Criar o DbContext (Data/AppDbContext)
+        public DbSet<Tarefa> Tarefas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configurações adicionais, se necessário
+            modelBuilder.Entity<Tarefa>()
+                .HasOne(t => t.Usuario)
+                .WithMany(u => u.Tarefas)
+                .HasForeignKey(t => t.UsuarioId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Models/Entities/Tarefa.cs
@@ -0,0 +1,21 @@
+﻿namespace ToDoList.Models.Entities
+{
+    public class Tarefa
+    {
+        public Guid Id { get; set; }
+
+        public string Titulo { get; set; } = string.Empty;
+        public string? Descricao { get; set; } = string.Empty;
+        public bool Concluida { get; set; } = false;
+
+        public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+        public DateTime? AtualizadaEm { get; set; } = DateTime.UtcNow;
+
+        //Relação com Usuario
+        //A tarefa pertence a um usuário, e um usuário pode ter várias tarefaAs
+        public Guid UsuarioId { get; set; }
+
+        public Usuario? Usuario { get; set; }
+
+    }
+}
